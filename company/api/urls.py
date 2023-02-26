b7fd679ecfcc803c41649976423faf7403af05c3
YaroslavYaryk <duhanov2003@gmail.com>
2023-02-26 16:38:02 +0200

    company profile, company images, business_stream api

--- company/api/urls.py
+++ company/api/urls.py
@@ -1,6 +1,22 @@
 from django.urls import path, include
-from .views import index
+from .views import CompanyAPIView, CompanyImagesAPIView, delete_all_company_images, BusinessStreamStreamAPIView
 
 urlpatterns = [
-    path("", index)
+    # company
+    path("companies/all/", CompanyAPIView.as_view(), name="get_all_companies"),
+    path("companies/one/<id>/", CompanyAPIView.as_view(), name="get_one_company"),
+    path("companies/create/", CompanyAPIView.as_view(), name="create_company"),
+    path("companies/<pk>/edit/", CompanyAPIView.as_view(), name="create_company"),
+    path("companies/<pk>/delete/", CompanyAPIView.as_view(), name="create_company"),
+
+    # company images
+    path("company/<pk>/images/", CompanyImagesAPIView.as_view(), name="get_company_images"),
+    path("company/<pk>/images/add/", CompanyImagesAPIView.as_view(), name="add_company_images"),
+    path("company/<pk>/images/add/", CompanyImagesAPIView.as_view(), name="add_company_images"),
+    path("company/<pk>/images/<image_id>/delete/", CompanyImagesAPIView.as_view(), name="delete_company_image"),
+    path("company/<pk>/images/delete-all/", delete_all_company_images, name="delete_all_company_images"),
+
+    #  BusinessStream
+    path("business-stream/all/", BusinessStreamStreamAPIView.as_view(), name="get_all_business_streams"),
+
 ]
--- company/models.py
+++ company/models.py
@@ -1,20 +1,23 @@
 from django.db import models
+from users.models import User
 
 
 # Create your models here.
 class Company(models.Model):
-    company_name = models.CharField(verbose_name="name", max_length=100)
-    profile_description = models.TextField(max_length=1000)
-    business_stream = models.ForeignKey("BusinessStream", on_delete=models.CASCADE)
-    establishment_date = models.DateField(verbose_name="establishment date")
-    company_website_url = models.CharField(verbose_name="website url", max_length=500)
+    company_name = models.CharField(verbose_name="name", max_length=100, null=True)
+    user_account = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    profile_description = models.TextField(max_length=1000, null=True)
+    business_stream = models.ForeignKey("BusinessStream", on_delete=models.CASCADE, null=True)
+    establishment_date = models.DateField(verbose_name="establishment date", null=True)
+    company_website_url = models.CharField(verbose_name="website url", max_length=500, null=True)
 
     def __str__(self):
         return f"name - {self.company_name}, establishment date - {self.establishment_date}"
 
 
 class BusinessStream(models.Model):
-    business_stream_name = models.CharField(verbose_name="name", max_length=100)
+    business_stream_name = models.CharField(verbose_name="name", max_length=100, null=True)
+    business_stream_english_name = models.CharField(verbose_name="English name", max_length=100, null=True)
 
     def __str__(self):
         return f"{self.business_stream_name}"
